d6fc1bb3a0cd5ecc6406cf42d4f8f625541c5a63
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-07 03:37:06 +0600

    add fontend .env file

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,23 +1,27 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import fetched_url  from "./urls";
+
+
+
+
 
 function Home() {
-  const [name, setname] = useState({});
+  const [name, setname] = useState("");
+
+  
   useEffect(() => {
     const fetched =async()=>{
-     await fetch(fetched_url)
+     await fetch(import.meta.env.VITE_urls)
         .then((res) => res.json())
         .then((data) => setname(data.name));
     } 
     fetched()
-
-    console.log(name , "this is first rendering");
     
   }, []);
+  
   return (
     <div> 
-      <h1 className="text-green-500 text-center text-bold">shaadfim </h1>
+      <h1 className="text-green-500 text-center text-bold uppercase font-semibold text-7xl">{name} </h1>
     </div>
   );
 }
--- frontend/src/component/urls.jsx
+++
@@ -1,3 +0,0 @@
-const fetched_url = "http://localhost:3000/"
-
-export default fetched_url
