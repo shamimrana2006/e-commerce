b6daca4e83cb1baf36c44c448b119bcc142b166b
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-07 13:59:39 +0600

    add loading with using usestate

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,28 +1,33 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
-
-
+import Loadings from "./loading/Loadings";
+// import Loadings from "./loading/Loadings";
 
 
 function Home() {
   const [name, setname] = useState("");
-  
-  
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-      console.log("shamim rana");
-    const fetched =async()=>{
-     await fetch(import.meta.env.VITE_urls)
+    console.log("shamim rana");
+    console.log(Boolean(loading));
+
+    const fetched = async () => {
+      await fetch(import.meta.env.VITE_urls)
         .then((res) => res.json())
         .then((data) => setname(data.name));
-    } 
-    fetched()
-    
+       setLoading(false)
+    };
+    fetched();
   }, []);
-  
+
   return (
-    <div> 
-      <h1 className="text-green-500 text-center text-bold uppercase font-semibold text-7xl">{name} <br /> <hr /> tui ki korbi kor </h1>
+    <div>
+      
+      {loading ? <Loadings /> : false }
+      <h1 className="text-green-500 text-center text-bold uppercase font-semibold text-7xl">
+        {name} <br /> <hr /> tui ki korbi kor{" "}
+      </h1>
     </div>
   );
 }
---
+++ frontend/src/component/loading/Loadings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loadings() {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center font-bold text-green-500'>
+        Loading...
+    </div>
+  )
+}
+
+export default Loadings
