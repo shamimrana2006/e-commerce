a11452540c080697ef64a2d18b8509a66ccfe9a5
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-09 18:12:19 +0600

    test a new router

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const { cors_setup } = require("./service/cors_setup");
 const { error_handle } = require("./routers/error_handle/error_handle");
 const { user_router } = require("./routers/users/user_router");
 const { seed_router } = require("./routers/users/seed_users_router");
+const { users_model } = require("./models/db_model/db_model");
+const { success_res } = require("./service/responsed_handlling");
 
 app.use(cors_setup);
 app.use(express.json());
@@ -11,6 +13,12 @@ app.use(express.urlencoded({extended: true}))
 app.get("/", (req, res) => {
   res.json({ name: "welcome our server" });
 });
+
+app.get("/shamim",async(req,res)=>{
+  const all_users = await users_model.find({})
+
+  success_res(res,{status_code: 200, payload: all_users})
+})
 app.use("/reset/",seed_router)
 app.use("/api/",user_router)
 app.use(error_handle);
