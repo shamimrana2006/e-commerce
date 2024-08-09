aac6ac2a2be98487febe5bb7b4ba67cf9000450f
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-09 18:01:44 +0600

    change reset route post to get

--- backend/routers/users/seed_users_router.js
+++ backend/routers/users/seed_users_router.js
@@ -3,7 +3,7 @@ const { seed_controller } = require("../../controller/user_constroller/seed_cont
 const seed_router = require("express").Router();
 
 // /api
-seed_router.post("/users/seed/",seed_controller)
+seed_router.get("/users/seed/",seed_controller)
 
 
 module.exports = {seed_router}
