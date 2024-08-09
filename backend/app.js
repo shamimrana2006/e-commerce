afb8318a0df8dcb286dd3df442bef7c2d9ca974d
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-09 21:54:56 +0600

    test shamim

--- backend/app.js
+++ backend/app.js
@@ -11,17 +11,12 @@ const { DB_connection } = require("./service/db_connection");
 app.use(cors_setup);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
   res.json({ name: "welcome our server" });
+  await  DB_connection();
 });
 
-app.get("/shamim",async(req,res)=>{
-  console.log("start");
-  await  DB_connection();
- const all_user = await users_model.find({"name": "billah khan"})
 
-  success_res(res,{status_code: 200, payload: all_user})
-})
 app.use("/reset/",seed_router)
 app.use("/api/",user_router)
 app.use(error_handle);
