8eca580fc10d900b0e487f29efe1822f89439a29
shamimrana2006 <mdshamimrana20@gmail.com>
2024-08-10 15:39:31 +0600

    add BrowserRouter in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./component/Home";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1 className="h-screen flex-col flex justify-center items-center w-screen bg-gray-900 text-white uppercase"> <span className="text-2xl">404</span> <br /> not found</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
